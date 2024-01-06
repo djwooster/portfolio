8f8b92b45ce32e825a8ef0b83588da1b8c91ce3f
DJ Wooster <142268182+djwooster@users.noreply.github.com>
2024-01-05 19:11:18 -1000

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+"strict mode";
+
+const clickMe = document.querySelector(".btn");
+
+clickMe.addEventListener("click", function () {
+  console.log("clicked");
+});
+
+const calculateSum = function (arr) {
+  for (let i = 0; i <= arr.length; i++) {
+    let sum = 0;
+  }
+};
